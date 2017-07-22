1773f4bac70f3be8f116d6eb41641afe26b6bc41
dabc4694 <darshan.shah130@gmail.com>
2017-07-22 01:56:29 -0500

    Used command line arguments to get the command

--- app.js
+++ app.js
@@ -14,24 +14,18 @@ So, when we download the files of the project use 'npm install' command
 in project folder.
 It will install necessary modules according to the dependencies in the package.json
 */
-
 const _ = require('lodash');
 
-// Examples:
-// 1: Check the the value is String or not
+var command = process.argv[2];
 
-var checkString_1 = _.isString(1);
-var checkString_Darshan = _.isString("Darshan");
-
-console.log("is 1 a String: "+checkString_1);
-console.log("is 'Darshan' a String: "+checkString_Darshan);
-
-// 2: Convert array to the set by eliminating duplicate values
-
-var array = ["Darshan", 1, "Shah", 1, "Darshan", 2, 3, 4, true, true];
-var arrayToSet = _.uniq(array);
-
-console.log("Initially the array is : " + array);
-console.log("After using 'uniq' ");
-console.log("New array ( converted to set ) : " + arrayToSet);
-
+if(command === 'add'){
+    console.log("Note added");
+} else if(command === 'list'){
+     console.log("List of notes");
+} else if(command === 'read'){
+    console.log("Reading note");
+} else if(command === 'remove'){
+    console.log("Removing note");
+} else{
+    console.log("Command not recognized");
+}
