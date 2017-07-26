79e190c45021ebef2bbe45615de958657c31f2fa
darshan4694 <darshan.shah4694@gmail.com>
2017-07-26 17:23:25 -0500

    addinf try catch blocks in reading file (to prevent if file doesn't exist

--- notes.js
+++ notes.js
@@ -8,9 +8,12 @@ var addNote = (title, body) => {
         body
     };
 
-    var notesDataString = fs.readFileSync('./data/notesData.json');
-    notes = JSON.parse(notesDataString);
+    try {
+        var notesDataString = fs.readFileSync('./data/notesData.json');
+        notes = JSON.parse(notesDataString);
+    } catch (e) {
 
+    }
     notes.push(note);
     fs.writeFileSync('./data/notesData.json', JSON.stringify(notes));
     console.log("Note Added:- ");
